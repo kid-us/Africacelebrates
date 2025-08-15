6ebf9e3acfbcee0ea52d0a995195966537f57533
kid-us <kidushh29@gmail.com>
2025-08-16 02:00:10 +0300

    Bold font for title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,9 +1,6 @@
 const SectionTitle = ({ label }: { label: string }) => {
   return (
-    <p
-      className="border border-primary/20 rounded-full px-4 py-2 w-52 text-center bg-secondary/8 mb-10 mx-5 lg:mx-0
-      bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text text-lg"
-    >
+    <p className="border border-primary/20 rounded-full px-4 py-2 w-52 text-center bg-secondary/8 mb-10 mx-5 lg:mx-0 font-semibold bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text text-lg">
       {label}
     </p>
   );
